7097c462da624672f7f86a13f8cc1cb9353912d9
Hiep Ba Le <hiepbalee@gmail.com>
2020-02-17 17:21:16 +0700

    vendor: Setting project and add firebase dependencies, spd + ssp dependencies, build mvp basic architecture

---
+++ app/src/main/java/com/csctd/appgrouptd/root/AppConstant.java
@@ -0,0 +1,7 @@
+package com.csctd.appgrouptd.root;
+
+public class AppConstant {
+    public static final String APP_NAME = "App Groups";
+    public static final String FIREBASE_REALTIME = "FIREBASE_REALTIME";
+    public static final String FIREBASE_FIRESTORE = "FIREBASE_FIRESTORE";
+}
